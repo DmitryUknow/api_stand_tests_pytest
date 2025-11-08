6647229766fbed097f01e93693cc94e45c1817f5
Dmitry <dmitrylive@mail.ru>
2025-11-08 11:13:23 +0300

    Second commit

--- create_user_test.py
+++ create_user_test.py
@@ -103,3 +103,5 @@ def test_create_user_number_type_first_name_get_error_response():
     user_body = get_user_body(12)
     response = sender_stand_request.post_new_user(user_body)
     assert response.status_code == 400
+
+    #------------Конец--------------
